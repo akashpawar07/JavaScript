7dfb9d44cef9f28bd31c1c5e452e82e5ead43c5f
Akash Pawar <akashshivrampawar@gmail.com>
2024-09-04 02:30:55 -0700

    js commit done

---
+++ nameShortner.js
@@ -0,0 +1,17 @@
+
+// function nameShortner(name, maxLength) {
+
+//     let oper1 = document.getElementById('demo').value;
+
+//     if (!name) { 
+//         document.getElementsByClassName('result')[0].innerHTML = "sorry!";
+//         return;
+//     } else if (name.length > maxLength) {
+//         return name.substring(0, maxLength) + "...";
+//     }
+//     return name;
+// }
+
+// let newNames = nameShortner(oper1, 8);
+// console.log(newNames);
+// document.getElementsByClassName('result')[0].innerHTML = newNames;
